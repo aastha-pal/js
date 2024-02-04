5c4ac54c03da8812661261f57e464a9d2b86a758
aastha-pal <112065229+aastha-pal@users.noreply.github.com>
2024-02-04 08:34:37 +0000

    commit 2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accoundId = 144553
+let accountEmail = "aastha@google.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+
+// accoundId = 2
+
+accountEmail="krish@google.com"
+accountPassword="23456"
+accountCity="Prayagraj"
+
+console.table([accoundId,accountEmail,accountPassword,accountCity])
